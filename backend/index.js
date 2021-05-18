d6ac427cee3163a04b2750d7a1e57cc28eb0ecc9
DNA5769 <dennisthomas2002@gmail.com>
2021-05-18 19:57:50 +0530

    Setup socket io in backend

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const http = require('http');
+const socket = require("socket.io");
 
 const app = express();
 const server = http.createServer(app);
+const io = socket(server);
 
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.send('<h1>Hello World!</h1>');
+});
+
+io.on('connection', (socket) => {
+  console.log('A user connected');
 });
 
 server.listen(PORT, () => console.log(`[SERVER] Started`));
