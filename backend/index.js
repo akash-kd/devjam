1d3e6a2b4dcda87d9fd33fddcfa1bdb18472f3aa
DNA5769 <dennisthomas2002@gmail.com>
2021-05-18 23:38:19 +0530

    Added creating/joining room functionality to sockets

--- backend/index.js
+++ backend/index.js
@@ -8,22 +8,52 @@ const io = socket(server);
 
 const PORT = process.env.PORT || 3000;
 
+app.use(express.urlencoded({ extended: false }));
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
   res.render('index');
 });
 
-app.get('/app', (req, res) => {
-  res.render('app');
+app.post('/app', (req, res) => {
+  const { name, type, room } = req.body;
+  res.render('app', { name, type, room });
 });
 
-io.on('connection', (socket) => {
-  console.log('[SOCKET] New client connected!');
+let users = [];
 
-  socket.on('disconnect', () => {
-    console.log('[SOCKET] Client disconnected!')
+io.on('connection', (socket) => {
+  // Every user on joining the app will send
+  // the name, room (code if any) and type (join/create) 
+  // to the server through this event "enter"
+  socket.on('enter', ({ name, room, type }) => {
+    console.log('[SOCKET] New client connected!');
+
+    // If the type is create, we have to create the room ourselves
+    // We also have to send it to the client so that it is aware about
+    // the room we created for it
+    if (type === 'create')
+    {
+      // Creating room
+      room = socket.id;
+
+      // Sending room to client
+      socket.emit('getRoom', room);
+    }
+
+    // Joining the socket to the room
+    socket.join(room);
+
+    // Will help us keep track of users
+    users.push({ name, room });
   });
+
+  // Helps us keep track of when the user disconnects
+  socket.on('disconnect', () => {
+    console.log('[SOCKET] Client disconnected!');
+  });
+
+  console.log(users);
 });
 
 server.listen(PORT, () => console.log(`[SERVER] Started!`));
