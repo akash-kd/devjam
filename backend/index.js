733791ddf37f4f4a127357a43055608a5830861e
Aqib Suhail <geeqib23@pop-os.localdomain>
2021-05-19 21:13:15 +0530

    enabled cors, backend connected to frontend

--- backend/index.js
+++ backend/index.js
@@ -1,43 +1,48 @@
-const express = require('express');
-const http = require('http');
-const socket = require('socket.io');
-const chatEvents = require('./socketEvents/chatEvents');
-const basicEvents = require('./socketEvents/basicEvents');
+const express = require('express')
+const http = require('http')
+const socket = require('socket.io')
+const chatEvents = require('./socketEvents/chatEvents')
+const basicEvents = require('./socketEvents/basicEvents')
 
-const app = express();
-const server = http.createServer(app);
-const io = socket(server);
+const app = express()
+const server = http.createServer(app)
+const io = socket(server, {
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
+  },
+})
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080
 
-app.use(express.urlencoded({ extended: false }));
-app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: false }))
+app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => {
-  res.render('index');
-});
+  res.render('index')
+})
 
 app.post('/app', (req, res) => {
-  const { name, type, room } = req.body;
-  res.render('app', { name, type, room });
-});
+  const { name, type, room } = req.body
+  res.render('app', { name, type, room })
+})
 
 // To store info about each user
-let users = [];
+let users = []
 
 // A hashmap to store number of users in each room
-let rooms = {};
+let rooms = {}
 
-io.on('connection', (socket) => {
+io.on('connection', socket => {
   // Takes care of basic events the socket has to
   // be setup for like when the user joins/leaves/etc.
-  basicEvents(socket, users, rooms);
+  basicEvents(socket, users, rooms)
 
   // Takes care of all chat events the socket has to
   // setup for like when the user sends/receives messages etc.
-  chatEvents(socket, users);
+  chatEvents(socket, users)
 
-  console.log(rooms);
-});
+  console.log(rooms)
+})
 
-server.listen(PORT, () => console.log(`[SERVER] Started!`));
+server.listen(PORT, () => console.log(`[SERVER] Started!`))
