c9988981ab4d7074e8a1876b1f2fa06b0ee7213e
DNA5769 <dennisthomas2002@gmail.com>
2021-05-19 15:09:02 +0530

    Added room validation

--- backend/index.js
+++ backend/index.js
@@ -22,16 +22,22 @@ app.post('/app', (req, res) => {
   res.render('app', { name, type, room });
 });
 
+// To store info about each user
 let users = [];
+
+// A hashmap to store number of users in each room
+let rooms = {};
 
 io.on('connection', (socket) => {
   // Takes care of basic events the socket has to
   // be setup for like when the user joins/leaves/etc.
-  basicEvents(socket, users);
+  basicEvents(socket, users, rooms);
 
   // Takes care of all chat events the socket has to
   // setup for like when the user sends/receives messages etc.
   chatEvents(socket, users);
+
+  console.log(rooms);
 });
 
 server.listen(PORT, () => console.log(`[SERVER] Started!`));
--- backend/socketEvents/basicEvents.js
+++ backend/socketEvents/basicEvents.js
@@ -1,4 +1,4 @@
-function basicEvents(socket, users)
+function basicEvents(socket, users, rooms)
 {
   // Every user on joining the app will send
   // the name, room (code if any) and type (join/create) 
@@ -13,6 +13,7 @@ function basicEvents(socket, users)
     {
       // Creating room
       room = `RO${socket.id}OM`;
+      rooms[room] = 1;
 
       // Sending room to client
       socket.emit('getRoom', room);
@@ -22,10 +23,24 @@ function basicEvents(socket, users)
     socket.join(room);
 
     // If the user is joining an already existing room
-    // We need to alert the other users already in the room
-    // that the user has joined. This event helps with that
+    // we need to make sure that the room the user is trying
+    // to join exists
     if (type === 'join')
-      socket.broadcast.to(room).emit('userJoin', name);
+    {
+      // This means that the room doesn't exist
+      if (typeof(rooms[room]) === "undefined")
+        socket.emit('validateRoom', false);
+      // This means that the room does exist
+      else
+      {
+        ++rooms[room];
+        socket.emit('validateRoom', true);
+
+        // We need to alert the other users already in the room
+        // that the user has joined. This event helps with that
+        socket.broadcast.to(room).emit('userJoin', name);
+      }
+    }
 
     // Will help us keep track of users
     users.push({ id: socket.id, name, room });
@@ -38,9 +53,16 @@ function basicEvents(socket, users)
 
     // Remove the user from the global users list
     let user = users.splice(users.findIndex(user => user.id === socket.id), 1)[0];
-    
-    // Tells all the other users in that room, that the user has left
-    socket.broadcast.to(user.room).emit('userLeave', user.name);
+
+    if (typeof(rooms[user.room]) !== "undefined")
+    {
+      // Tells all the other users in that room, that the user has left
+      socket.broadcast.to(user.room).emit('userLeave', user.name);
+
+      rooms[user.room]--;
+      if (!rooms[user.room])
+        delete rooms[user.room];
+    }
   });
 }
 
