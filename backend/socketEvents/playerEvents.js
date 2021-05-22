60dfcace6cf6fa52f83c9fd70a9ad05360a722a8
Aqib Suhail <geeqib23@pop-os.localdomain>
2021-05-22 21:07:31 +0530

    player controls not visible to new admin fixed

--- backend/socketEvents/playerEvents.js
+++ backend/socketEvents/playerEvents.js
@@ -1,23 +1,28 @@
 function playerEvents(socket, users, rooms) {
   socket.on('sendInfo', data => {
-    let user = users.find(user => user.id === socket.id);
-    socket.broadcast.to(user.room).emit('getInfo', data);
-  });
+    let user = users.find(user => user.id === socket.id)
+    if (user) socket.broadcast.to(user.room).emit('getInfo', data)
+  })
 
   socket.on('urlChanged', url => {
-    let user = users.find(user => user.id === socket.id);
-    socket.broadcast.to(user.room).emit('urlChange', url);
-  });
+    let user = users.find(user => user.id === socket.id)
+    if (user) socket.broadcast.to(user.room).emit('urlChange', url)
+  })
 
   socket.on('paused', () => {
-    let user = users.find(user => user.id === socket.id);
-    socket.broadcast.to(user.room).emit('pause');
-  });
+    let user = users.find(user => user.id === socket.id)
+    if (user) socket.broadcast.to(user.room).emit('pause')
+  })
 
-  socket.on('played', () => {
-    let user = users.find(user => user.id === socket.id);
-    socket.broadcast.to(user.room).emit('play');
-  });
+  socket.on('played', currentTime => {
+    let user = users.find(user => user.id === socket.id)
+    if (user) socket.broadcast.to(user.room).emit('play', currentTime)
+  })
+
+  socket.on('seeked', currentTime => {
+    let user = users.find(user => user.id === socket.id)
+    if (user) socket.broadcast.to(user.room).emit('seek', currentTime)
+  })
 }
 
 module.exports = playerEvents
--- frontend/src/components/chatbox.js
+++ frontend/src/components/chatbox.js
@@ -5,13 +5,15 @@ import '../styles/main.css'
 //Components
 import Textinput from '../components/textInput.js'
 import Chatbit from '../components/chatbit.js'
-import { useContext, useEffect, useState } from 'react'
+import { createContext, useContext, useEffect, useState } from 'react'
 import { useImmer } from 'use-immer'
 import StateContext from '../StateContext'
+import SocketContext from '../SocketContext'
 import Button from './button'
 
-function Chatbox({ socket }) {
+function Chatbox() {
   const appState = useContext(StateContext)
+  const socket = useContext(SocketContext)
   const [state, setState] = useImmer({
     chatMessage: [],
     textField: '',
@@ -25,57 +27,84 @@ function Chatbox({ socket }) {
   }
 
   function handleSubmit(e) {
-    console.log(state.textField)
+    console.log(appState)
     e.preventDefault()
     //sending message to server
-    if (state.textField) socket.emit('chatSend', state.textField)
-    //making the textField empty after sending
+    if (state.textField) {
+      socket.emit('chatSend', state.textField)
+      //making the textField empty after sending
+      setState(draft => {
+        draft.chatMessage.push({ text: state.textField, name: appState.user.username })
+        draft.textField = ''
+      })
+    }
+  }
+
+  const handleChatReceive = ({ name, message }) => {
+    console.log(name, message)
     setState(draft => {
-      draft.chatMessage.push({ text: state.textField, name: appState.user.username })
-      draft.textField = ''
+      draft.chatMessage.push({ text: message, name })
+    })
+  }
+
+  const handleUserJoin = name => {
+    //   console.log(`${name} has joined the room!</div>`)
+    //add message to state collection of message
+    setState(draft => {
+      draft.chatMessage.push({ text: `${name} has joined the room!` })
+    })
+  }
+
+  const handleUserLeave = name => {
+    //   console.log(`${name} has left the room!</div>`)
+    setState(draft => {
+      draft.chatMessage.push({ text: `${name} has left the room!` })
+    })
+  }
+
+  const handleAdminLeave = ({ name, new_admin }) => {
+    setState(draft => {
+      draft.chatMessage.push({ text: `${name} has left the room! ${new_admin.name} is the new admin` })
     })
   }
 
   useEffect(() => {
-    socket.on('chatReceive', ({ name, message }) => {
-      console.log(name, message)
-      setState(draft => {
-        draft.chatMessage.push({ text: message, name })
-      })
-    })
+    socket.on('chatReceive', handleChatReceive)
 
-    socket.on('userJoin', name => {
-      //   console.log(`${name} has joined the room!</div>`)
-      //add message to state collection of message
-      setState(draft => {
-        draft.chatMessage.push({ text: `${name} has joined the room!` })
-      })
-    })
+    socket.on('userJoin', handleUserJoin)
 
-    socket.on('userLeave', name => {
-      //   console.log(`${name} has left the room!</div>`)
-      setState(draft => {
-        draft.chatMessage.push({ text: `${name} has left the room!` })
-      })
-    })
+    socket.on('userLeave', handleUserLeave)
+
+    socket.on('adminLeave', handleAdminLeave)
+
+    return () => {
+      // socket.off('adminLeave', handleAdminLeave)
+      socket.off('chatReceive', handleChatReceive)
+      socket.off('userJoin', handleUserJoin)
+      socket.off('userLeave', handleUserLeave)
+      socket.off('adminLeave', handleAdminLeave)
+    }
   }, [])
   return (
-    <div className='chatbox flex flex-col w-full ml-3 p-3 bg-gray-900 items-center'>
-      <a className='text-lg underline'>Chat</a>
+    <div className="flex h-full w-full justify-center items-center">
+      <div className='flex flex-col h-full w-full ml-3 p-3 bg-gray-900 items-center'>
+        <a className='text-lg underline'>Chat</a>
 
-      <div className='flex flex-col flex-grow w-full overflow-y-auto'>
-        {state.chatMessage.map(({ text, name }, index) => {
-          return (
-            <div key={index}>
-              <Chatbit name={name} text={text} />
-            </div>
-          )
-        })}
+        <div className='flex flex-col flex-grow w-full overflow-y-auto'>
+          {state.chatMessage.map(({ text, name }, index) => {
+            return (
+              <div key={index}>
+                <Chatbit name={name} text={text} />
+              </div>
+            )
+          })}
+        </div>
+        <form className="flex flex-row w-full">
+          <Textinput value={state.textField} onChange={e => updateField(e)} placeholder='Type your message here' />
+          <button type='submit' className="flex shadow-md w-20 items-center justify-center text-white py-2 px-2 my-2 ml-2 text-center border-2 rounded-md border-indigo-600 bg-indigo-500" onClick={handleSubmit}>Send</button>
+          
+        </form>
       </div>
-      <form>
-        <Textinput value={state.textField} onChange={e => updateField(e)} placeholder='Type your message here' />
-        <Button type='submit' text='Send' onClick={handleSubmit} />
-      </form>
     </div>
   )
 }
